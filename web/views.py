6fd026003a47859f529706cf91bc3de82fa16763
Koh Huan Yin (David) <alphonsekoh@gmail.com>
2022-09-22 03:17:12 +0800

    wip: Added navbar

--- web/views.py
+++ web/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
 from django.template import loader
-from dotenv import load_dotenv
-import pymongo
-import os
-
-load_dotenv()
-mongo_connection_string = os.getenv('MONGO_CONNECTION_STRING')
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('base.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'home.html')
 
